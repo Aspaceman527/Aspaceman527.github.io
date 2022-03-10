d67759e64980d305bc158f3dc98623a99a4d21d5
Aspaceman527 <92396467+Aspaceman527@users.noreply.github.com>
2022-03-10 11:36:28 -0500

    Update clicky.js

--- clicky-clicker/clicky.js
+++ clicky-clicker/clicky.js
@@ -1,1 +1,21 @@
+clickCounter();
 
+function clickCounter() {
+  if (localStorage.clickcount) {
+    localStorage.clickcount = Number(localStorage.clickcount)+1;
+  } else {
+    localStorage.clickcount = 1;
+  }
+  document.getElementById("clicks").innerHTML = localStorage.clickcount;
+}
+/*cps();
+
+function cps() {
+  if (localStorage.b1) {
+    localStorage.b1 = Number(localStorage.b1)+1;
+  } else {
+    localStorage.b1 = 1;
+  }
+   document.getElementById('b1').innerHTML = localStorage.b1;
+ }
+    */
