dc64932316a71ff45f504be9feffb41edfb13d19
Aspaceman527 <92396467+Aspaceman527@users.noreply.github.com>
2022-03-10 11:04:50 -0500

    Create script.js

---
+++ weird-game/script.js
@@ -0,0 +1,9 @@
+let cars = 1
+let driving = false
+let player = 1
+// will work on skins later
+while (driving === true) {
+ let harddrivingthing = true
+} else {
+  let harddrivingthing = false
+  }
