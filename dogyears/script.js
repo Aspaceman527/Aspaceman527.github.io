4398b3031a0f3b3e8a01fd8a9844fb3396a16073
Aspaceman527 <92396467+Aspaceman527@users.noreply.github.com>
2022-03-15 08:06:21 -0400

    Update script.js

--- dogyears/script.js
+++ dogyears/script.js
@@ -1,8 +1,11 @@
-const myAge = document.getElementById("Age");
+var myAge = document.getElementById("Age");
 //age
 let earlyYears = 2;
 earlyYears *= 10.5;
 let laterYears = myAge - 2;
 laterYears *= 4;
-var dogeYears = earlYears + laterYears;
+var dogeYears = earlyYears + laterYears;
 window.alert(`your age in dog years is ${dogeYears} years old`)
+/*
+copyright Aspaceman527
+*/
